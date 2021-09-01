37ecf28b025e7b0d32a0f1666e30fee202798247
unknown <ramirezgonzalomauricio@gmail.com>
2021-08-31 23:19:09 -0300

    Updates

---
+++ precache-manifest.ccd8b9665e8a7dde1c0d4c63265baaaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf5d395748bb263efae93f7c2a6c457",
+    "url": "/ecommerce-gramirez/index.html"
+  },
+  {
+    "revision": "dd63a013700c9db0f7d4",
+    "url": "/ecommerce-gramirez/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "afac18860af770707c18",
+    "url": "/ecommerce-gramirez/static/css/main.6f0bc612.chunk.css"
+  },
+  {
+    "revision": "dd63a013700c9db0f7d4",
+    "url": "/ecommerce-gramirez/static/js/2.5ddcb636.chunk.js"
+  },
+  {
+    "revision": "2520f30cb4e102284a8f5fa3a821a3bf",
+    "url": "/ecommerce-gramirez/static/js/2.5ddcb636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afac18860af770707c18",
+    "url": "/ecommerce-gramirez/static/js/main.538df2d0.chunk.js"
+  },
+  {
+    "revision": "aa181c13d42eea6ea8d9",
+    "url": "/ecommerce-gramirez/static/js/runtime-main.efb1ff33.js"
+  },
+  {
+    "revision": "cd22f79e9359f81c6db188311c01fd01",
+    "url": "/ecommerce-gramirez/static/media/CartIcon.cd22f79e.svg"
+  },
+  {
+    "revision": "4135c94e88f348575bfac94972cf32ea",
+    "url": "/ecommerce-gramirez/static/media/loadingSpin.4135c94e.svg"
+  }
+]);
